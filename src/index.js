0aa4312ef1ca318cf8b22db28b26acb6dacc8536
hks0827 <p90p90p09@gmail.com>
2024-07-18 06:58:23 +0900

    게시판1차

--- src/index.js
+++ src/index.js
@@ -1,20 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-          <App />
-    </React.StrictMode>
-  </BrowserRouter>
-);
+searchEl.addEventListener('click', function () {
+  searchInputEl.focus();
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+searchInputEl.addEventListener('focus', function () {
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+searchInputEl.addEventListener('blur', function () {
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
+});
+
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function () {
+  console.log('scroll!');
+  if (window.scrollY > 500) {
+    //배지 없애기 
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'  
+    });
+  } 
+  else {
+    //배지 보이기
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+     });
+  }
+}, 300));
+// _.throttle(함수, 시간)
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function(fadeEl, index){
+  //gsap.to(요소, 지속시간, 옵션);
+  gsap.to(fadeEl,1,{
+    delay: (index + 1) * .7,
+    opacity:1
+  });
+});
