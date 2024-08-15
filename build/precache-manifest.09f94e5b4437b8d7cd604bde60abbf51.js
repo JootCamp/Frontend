747baa974a911938460f04a74a1382b0958fe20c
hks0827 <p90p90p09@gmail.com>
2024-08-15 13:58:39 +0900

    Add build folder to repository

---
+++ build/precache-manifest.09f94e5b4437b8d7cd604bde60abbf51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfcf1059d9e459216267d3eab0d48245",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63a151fcee260e762329",
+    "url": "/static/css/main.d1f45c59.chunk.css"
+  },
+  {
+    "revision": "07ec3776d70d7c98b6d2",
+    "url": "/static/js/2.a961cc80.chunk.js"
+  },
+  {
+    "revision": "65a9926ecde5afbe3bda",
+    "url": "/static/js/3.3b6fb9ae.chunk.js"
+  },
+  {
+    "revision": "63a151fcee260e762329",
+    "url": "/static/js/main.551d20dc.chunk.js"
+  },
+  {
+    "revision": "8d17c9e0d9526c2c8484",
+    "url": "/static/js/runtime~main.360358fb.js"
+  }
+]);
